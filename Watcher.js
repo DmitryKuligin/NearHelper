3b4fe96d7b110bca3f911401299a95c0c994d64b
Дмитрий Кулигин <46721224+DmitryKuligin@users.noreply.github.com>
2023-11-13 01:54:36 +0300

    fx

--- Watcher.js
+++ Watcher.js
@@ -1,5 +1,5 @@
 const Telegraf	= require('telegraf') 				// telegram bot api 
-const bot 		= new Telegraf('6842368538:AAE0ox_h9AqqBYVgw3e144FIh4Q_ZmGqDs4') 
+const bot 		= new Telegraf(process.env.'6842368538:AAE0ox_h9AqqBYVgw3e144FIh4Q_ZmGqDs4') 
 const nearApi 	= require('near-api-js') 			// near api
 const level  	= require('level') 					// on-disk key-value store
 const accounts	= level('./accounts')
